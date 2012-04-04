d7923495d23f77dc8f360f34b4c12df943cc305f
Andrew Tulloch <ajtulloch@gmail.com>
2012-04-04 17:07:14 +0800

    Used context managers to load files (more Pythonic).  Also removed empty else  branches to clean up the code a bit.

--- suffdict_creation/suffdict.py
+++ suffdict_creation/suffdict.py
@@ -21,40 +21,33 @@ entries = cmudict.entries()
 
 
 def suff(dict):
-    f = open('suff_a.txt', 'a')
-    for word, vals in dict.iteritems():
-        if re.search("((?i)[BCDFGHJKLMNPQRSTVWXZ]{1,2}[AEIOUY]+[BCDFGHJKLMNPQRSTVWXZ]*(E|ED)?('[A-Z]{1,2})?)(?![a-zA-Z]+)", word):
-            graphemes = re.search("((?i)[BCDFGHJKLMNPQRSTVWXZ]{1,2}[AEIOUY]+[BCDFGHJKLMNPQRSTVWXZ]*(E|ED)?('[A-Z]{1,2})?)(?![a-zA-Z]+)", word).group()
-        else:
-            pass
-        val = min(vals, key=len)
-        # for val in vals:
-        i = -1
-        while i >= 0 - len(val):
-            if isdigit(val[i][-1]):
-                str = " ".join(val[i:])
-                f.write(graphemes + ' ' + str + '\n')
-                f.write(graphemes[1 - len(graphemes):] + ' ' + str + '\n')
-                break
-            i -= 1
-    f.close()
+    with open('suff_a.txt', 'a') as f:        
+        for word, vals in dict.iteritems():
+            if re.search("((?i)[BCDFGHJKLMNPQRSTVWXZ]{1,2}[AEIOUY]+[BCDFGHJKLMNPQRSTVWXZ]*(E|ED)?('[A-Z]{1,2})?)(?![a-zA-Z]+)", word):
+                graphemes = re.search("((?i)[BCDFGHJKLMNPQRSTVWXZ]{1,2}[AEIOUY]+[BCDFGHJKLMNPQRSTVWXZ]*(E|ED)?('[A-Z]{1,2})?)(?![a-zA-Z]+)", word).group()
+            val = min(vals, key=len)
+            # for val in vals:
+            i = -1
+            while i >= 0 - len(val):
+                if isdigit(val[i][-1]):
+                    str = " ".join(val[i:])
+                    f.write(graphemes + ' ' + str + '\n')
+                    f.write(graphemes[1 - len(graphemes):] + ' ' + str + '\n')
+                    break
+                i -= 1
 
 
 def most_prob(file):
     uniq_suffs = []
-    goal = open('suff_c.txt', 'a')
-    with open(file) as f:
-        for line in f:
-            suff = re.search("\s[a-zA-Z']+\s", line).group()
-            if suff not in uniq_suffs:
-                uniq_suffs.append(suff)
-                new_line = re.sub("\d+\s(?=[a-z])", "", line)
-                new_line = re.sub("(?<=[a-z])\s(?=[A-Z])", " 1 ", new_line).strip()
-                goal.write(new_line + '\n')
-            else:
-                pass
-    goal.close()
-
+    with open('suff_c.txt', 'a') as goal:
+        with open(file) as f:
+            for line in f:
+                suff = re.search("\s[a-zA-Z']+\s", line).group()
+                if suff not in uniq_suffs:
+                    uniq_suffs.append(suff)
+                    new_line = re.sub("\d+\s(?=[a-z])", "", line)
+                    new_line = re.sub("(?<=[a-z])\s(?=[A-Z])", " 1 ", new_line).strip()
+                    goal.write(new_line + '\n')
 
 # First, I grabbed all last syllable graphones:
 # suff(cmu)
